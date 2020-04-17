33086c2a9437a7a3369ddf46aee86c67f0762ae8
Danny Ramos <danramos21@gmail.com>
2020-04-17 17:20:56 -0600

    Keys skeleton

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,8 @@
 class Enigma
+
+  def initialize
+  end
+
+  def encrypt(message, key, date)
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'minitest/pride'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
@@ -10,5 +9,10 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_can_encrypt
+    expected = {encryption: "keder ohulw", key: "02715",date: "040895"}
+    assert_equal expected , @enigma.encrypt("hello world", "02715", "040895")
   end
 end
