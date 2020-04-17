f65cd4f83e6145ac1ae38d5f037509ac3403b803
Danny Ramos <danramos21@gmail.com>
2020-04-16 21:09:54 -0600

    setup enigma class

---
+++ test/enigma_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
