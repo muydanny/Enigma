005ce83781a7f522b8b79a63fcdbe0cc19cd3058
Danny Ramos <danramos21@gmail.com>
2020-04-18 21:40:59 -0600

    Current  date change

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,9 +11,19 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_encrypt_without_date
+    expected = {encryption: "pib wdmczpu", key: "02715", date: Time.now.strftime("%d%m%y")}
+    assert_equal expected , @enigma.encrypt("hello world", "02715")
+  end
+
   def test_it_can_encrypt
-    expected = {encryption: "keder ohulw", key: "02715",date: "040895"}
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
     assert_equal expected , @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_decrypt_without_date
+    expected = {encryption: "hello world", key: "02715", date: Time.now.strftime("%d%m%y")}
+    assert_equal expected , @enigma.decrypt("keder ohulw", "02715")
   end
 
   def test_it_can_decrypt
