1c474c20fdefb554a065b445b1868985492edf8e
Danny Ramos <danramos21@gmail.com>
2020-04-20 14:16:18 -0600

    Passing encrypt txt file

--- encrypt.rb
+++ encrypt.rb
@@ -1,13 +1,19 @@
+require './lib/enigma'
+
 handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
-
+incoming_text = incoming_text.gsub("\n", "")
 handle.close
 
-capitalized_text = incoming_text.upcase
+
+enigma = Enigma.new
+encrypted_message = enigma.encrypt(incoming_text)
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(capitalized_text)
+writer.write(encrypted_message[:encryption])
 
 writer.close
+
+puts "Created #{ARGV[0]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
