0b71b9e02e71a89358ae1e9d23be8124381a0e5b
Danny Ramos <danramos21@gmail.com>
2020-04-20 17:00:15 -0600

    Passing decrypt file io

--- decrypt.rb
+++ decrypt.rb
@@ -10,8 +10,8 @@ handle.close
 
 
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(incoming_text)
-decrypted_message = enigma.decrypt(incoming_text)
+# encrypted_message = enigma.encrypt(incoming_text)
+decrypted_message = enigma.decrypt(incoming_text, ARGV[2], ARGV[3])
 
 writer = File.open(ARGV[1], "w")
 
@@ -19,4 +19,4 @@ writer.write(decrypted_message[:decryption])
 
 writer.close
 
-puts "Created #{ARGV[0]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+puts "Created #{ARGV[1]} with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
